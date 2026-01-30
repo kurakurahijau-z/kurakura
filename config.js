b24711e4c89ad1188098b792e3eab0464da09308
kurakurahijau-z <mfar_80@yahoo.com>
2026-01-31 07:38:39 +0800

    Add global configuration in config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/* =========================================
+   KURA KURA HIJAU – GLOBAL CONFIG
+   Satu tempat sahaja simpan config penting
+   ========================================= */
+
+window.APP_CONFIG = {
+  BASE_URL: "https://script.google.com/macros/s/AKfycbzeD9VIaDzvbtllYJSWuk7duqEMCdDfXJE8zakhe_S_-5h4cQe9180RaEh77fDuq5IheQ/exec",
+  API_KEY: "PPF-KEY-2026-FAIZAL-SECRET"
+};
