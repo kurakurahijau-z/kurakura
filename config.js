2099e9444a16e8dada420e5f2bcc8e0ad26e3a18
kurakurahijau-z <mfar_80@yahoo.com>
2026-01-31 23:40:21 +0800

    Remove APP_CONFIG from config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-// config.js
-window.APP_CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbzeD9VIaDzvbtllYJSWuk7duqEMCdDfXJE8zakhe_S_-5h4cQe9180RaEh77fDuq5IheQ/exec",
-  API_KEY: "PPF-KEY-2026-FAIZAL-SECRET",
-  TOKEN_KEY: "kk_token"
-};
 
-console.log("✅ APP_CONFIG loaded:", window.APP_CONFIG);
