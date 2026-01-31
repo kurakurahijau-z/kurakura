1b71188a4c48263c01949ff24cbdc16c1a35c225
kurakurahijau-z <mfar_80@yahoo.com>
2026-01-31 09:40:52 +0800

    Add TOKEN_KEY to APP_CONFIG in config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@
 window.APP_CONFIG = {
   BASE_URL: "https://script.google.com/macros/s/AKfycbzeD9VIaDzvbtllYJSWuk7duqEMCdDfXJE8zakhe_S_-5h4cQe9180RaEh77fDuq5IheQ/exec",
   API_KEY: "PPF-KEY-2026-FAIZAL-SECRET"
+   // Token storage key (WAJIB sama untuk semua page)
+  TOKEN_KEY: "kk_token"
 };
