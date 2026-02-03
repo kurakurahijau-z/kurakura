20207aa12735fefefb6590316cea69de2d1eca33
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-03 22:32:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-// config.js
-window.APP_CONFIG = {
-  // PASTE URL WEB APP /exec KAT SINI (bukan /dev)
-  // contoh: "https://script.google.com/macros/s/XXXXXX/exec"
-  BASE_URL: "https://script.google.com/macros/s/AKfycbybvP8q-qNNsVvjbG-MR0KJRmx5qks21gAU_cwJrhJLfF0Vg8HsdD3Avs1Rmz5MUHEo/exec",
+// config.js (GLOBAL)
+// NOTE: GitHub Pages mesti guna script.googleusercontent.com untuk elak CORS
 
-  APP_NAME: "kura kura hijau"
+window.APP_CONFIG = {
+  APP_NAME: "kura kura hijau",
+
+  // ✅ guna URL yang kau bagi (googleusercontent)
+  BASE_URL: "https://script.googleusercontent.com/a/macros/pms.edu.my/echo?user_content_key=AehSKLiHNSjfftnaz7FyvBp8SKYkh3T0Pg50PVJYSXODBkWixSlqMUN7wuEqg_8pd5AhRlQSQHYmhVmpWrM1d2QnZCMINZQ6dO1D5FN-b3_Cu5xwv4K4dZSVNXQ49JmtZMSDH3c-bV9sh-wsqgDRjWrScOur-W4E3URuZJhB28GCodabXq-z5giVRzAWEotjU9CLobw_BWzyYO1sGq4yly7IpHpiqGmKSqmoiNr2G0CDp2EDy5BQNnHNlxMc9LDP7kmwcJb47ejYYPiHtwmyK6eBhXlPqa-VtaesARkPuS72GFKivpyOglBzCO-xgs8Uv6S6ZJDgAubw&lib=MQszmHW-vmYq61jNBMPlZPo147xIduJpS"
 };
