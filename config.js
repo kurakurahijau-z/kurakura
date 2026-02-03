29e81fd5195c73aa6c035c72399cd8498061eafe
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-04 00:15:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-// config.js (GLOBAL)
-// NOTE: GitHub Pages mesti guna script.googleusercontent.com untuk elak CORS
-
 window.APP_CONFIG = {
   APP_NAME: "kura kura hijau",
-
-  // ✅ guna URL yang kau bagi (googleusercontent)
   BASE_URL: "https://script.google.com/macros/s/AKfycbybvP8q-qNNsVvjbG-MR0KJRmx5qks21gAU_cwJrhJLfF0Vg8HsdD3Avs1Rmz5MUHEo/exec"
 };
