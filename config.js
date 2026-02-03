890f69398cf6ce12b336522c2bd33dba15cb2659
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-03 17:04:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
+window.APP_CONFIG = {
+  // PASTE URL WEB APP /exec KAT SINI (bukan /dev)
+  BASE_URL: "PASTE_URL_EXEC_KAT_SINI",
 
+  // optional: kalau kau ada protect token storage etc.
+  APP_NAME: "kura kura hijau"
+};
