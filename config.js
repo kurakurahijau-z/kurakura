0a162c415da2cca8d8b6aa7e7fb9fd7318d5b09f
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-04 12:26:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ window.APP_CONFIG = {
   APP_NAME: "kura kura hijau",
   BASE_URL: "https://script.google.com/macros/s/AKfycbybvP8q-qNNsVvjbG-MR0KJRmx5qks21gAU_cwJrhJLfF0Vg8HsdD3Avs1Rmz5MUHEo/exec"
 };
+
