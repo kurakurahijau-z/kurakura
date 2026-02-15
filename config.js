b38af6e372ec271734ccdb67ce4b3e8b688fca50
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-16 00:26:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-// config.js
 window.APP_CONFIG = {
-  APP_NAME: "kura kura hijau",
-
-  // ✅ Google Apps Script Web App URL (deploy kau)
   BASE_URL: "https://script.google.com/macros/s/AKfycbxTOhwbSkTgHoMvrp3EMRtKJTWm4UlddGlySl0pNbN4ytM2M0PhvAbpWd_JI0g3IR6H/exec",
-
-  // ✅ GitHub Pages repo path (PENTING supaya link tak lari)
-  // contoh URL kau: https://kurakurahijau-z.github.io/kurakura/...
   APP_BASE: "/kurakura"
 };
