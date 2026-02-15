23cfe67acb994cf0bd5b11d74fd1e3151f46450b
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-15 18:05:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 // config.js
-// ✅ Satu tempat je simpan URL Google Apps Script Web App kau
-// ✅ Semua page (admin + pelajar) akan ambil BASE_URL dari sini
-
 window.APP_CONFIG = {
-  // Tukar ini kalau kau redeploy Apps Script (URL baru)
+  // Google Apps Script Web App URL
   BASE_URL: "https://script.google.com/macros/s/AKfycbxTOhwbSkTgHoMvrp3EMRtKJTWm4UlddGlySl0pNbN4ytM2M0PhvAbpWd_JI0g3IR6H/exec",
 
-  // optional: versi untuk debug (tak wajib)
-  APP_NAME: "kura kura hijau",
-  ENV: "prod"
+  // GitHub Pages project base path (repo name)
+  BASE_PATH: "/kurakura"
 };
