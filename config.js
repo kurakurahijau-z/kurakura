626c0e736b57a96d5cfffe517498ac54941c4b73
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-16 21:00:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
 window.APP_CONFIG = {
+  // PASTE URL Web App Apps Script kau (yang /exec)
   BASE_URL: "https://script.google.com/macros/s/AKfycbzyd3baWT8RFojuWhjKCbmbSTaQoTlDzpeVsa9sf9jwkodGEXv7BXS38BqU1ltASis/exec"
 };
