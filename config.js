ad0121f62b58b35360b1fd5c3dd68468c2efdac6
kurakurahijau-z <mfar_80@yahoo.com>
2026-02-16 14:26:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
+// config.js
 window.APP_CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbzyd3baWT8RFojuWhjKCbmbSTaQoTlDzpeVsa9sf9jwkodGEXv7BXS38BqU1ltASis/exec",
-  APP_BASE: "/kurakura"
+  // WAJIB: URL web app Apps Script yang end dengan /exec
+  BASE_URL: "https://script.google.com/macros/s/AKfycbzyd3baWT8RFojuWhjKCbmbSTaQoTlDzpeVsa9sf9jwkodGEXv7BXS38BqU1ltASis/exec"
 };
